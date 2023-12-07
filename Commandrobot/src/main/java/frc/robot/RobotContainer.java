98424c2b0c019b7966706a58f126fe6ba5c6c1f2
z57333 <73370638+z57333@users.noreply.github.com>
2023-12-06 16:55:46 -0800

    new stuff

--- Commandrobot/src/main/java/frc/robot/RobotContainer.java
+++ Commandrobot/src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,50 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CameraServerCvJNI;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.macro.LimelightAlign;
+import frc.robot.commands.macro.ResetEncoders;
+import frc.robot.commands.macro.SetDeport;
+import frc.robot.commands.macro.SetHigh;
+import frc.robot.commands.macro.SetHighCube;
+import frc.robot.commands.macro.SetHome;
+import frc.robot.commands.macro.SetLowConeTilted;
+import frc.robot.commands.macro.SetLowCube;
+import frc.robot.commands.macro.SetMid;
+import frc.robot.commands.macro.SetMidCube;
+import frc.robot.commands.macro.timed.PIDTurnTimed;
+import frc.robot.commands.macro.SetHumanPlayer;
+import frc.robot.commands.macro.SetLowConeStanding;
+import frc.robot.commands.persistent.Elevate;
+import frc.robot.commands.persistent.MoveArmJoystick;
+import frc.robot.commands.persistent.MoveWristManual;
+import frc.robot.commands.persistent.RollClaw;
+import frc.robot.commands.persistent.SwerveDriveCmd;
+// import frc.robot.commands.auton.AutonBalance;
+import frc.robot.commands.auton.*;
+// import frc.robot.commands.persistent.VibrateControllerTimed;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.subsystems.Wrist;
+import frc.robot.subsystems.SwerveDrive.Direction;
+import frc.robot.subsystems.LEDStrip;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,13 +55,19 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer {
+public final class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Wrist wrist = new Wrist();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  Timer timer;
+	double last = 0.0;
+	double[] desiredStates = new double[13];
+
+	//Controllers
+	private final XboxController driver = new XboxController(0);
+	private final XboxController operator = new XboxController(1);
+	XboxController[] controllers = {driver, operator};
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -48,7 +91,8 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    wrist.setDefaultCommand(new MoveWristManual(wrist, operator));
+    new JoystickButton(operator, RobotMap.Controller.A).whenHeld(new WristMovement(wrist));
   }
 
   /**
--- Commandrobot/src/main/java/frc/robot/subsystems/Wrist.java
+++ Commandrobot/src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,23 +1,36 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Spark;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxRelativeEncoder;
+
+import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+import frc.robot.Constants;
 
 public class Wrist extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
 
-  private final Spark wristMotor = new Spark(5);
-  private final Encoder wristEncoder = new Encoder(5, 6);
-  private final double kEncoderTick2Meter = 1.0 / 4096.0 * 0.128 * Math.PI;
+  private final CANSparkMax wrist = new CANSparkMax(RobotMap.Wrist.WRIST, MotorType.kBrushless);
+    
   private PIDController wristPIDController = new PIDController(Constants.Wrist.P, Constants.Wrist.I, Constants.Wrist.D);
+  
+  private RelativeEncoder wristEncoder = wrist.getEncoder();
+
+  public Wrist() {
+    wrist.restoreFactoryDefaults();
+
+        wrist.setInverted(false);
+
+        wrist.setSmartCurrentLimit(999);
+        resetWristEncoder();
+  }
 
   public double getEncoderValues() {
     return (wristEncoder.get() * kEncoderTick2Meter);
@@ -40,27 +53,6 @@ public class Wrist extends SubsystemBase {
     wrist.stopMotor();
   }
 
-  public Wrist() {}
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  // public CommandBase exampleMethodCommand() {
-  //   // Inline construction of command goes here.
-  //   // Subsystem::RunOnce implicitly requires `this` subsystem.
-  //   return runOnce(
-  //       () -> {
-  //         /* one-time action goes here */
-  //       });
-  // }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
   public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
